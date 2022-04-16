38ebc9186561a855982b115e3b99daf962899ef5
EriqueRocha <eriquebit@gmail.com>
2022-04-16 01:04:43 -0300

    feat: armazena hora, minutos, intervalo feat: botão muda ao ser acionado e desligado,armazenando ou limpando chaves

--- app/src/main/java/com/example/praticandomais/MainActivity.java
+++ app/src/main/java/com/example/praticandomais/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.praticandomais;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -9,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnNotify;
@@ -18,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private int hour;
     private int minute;
     private int interval;
+    private boolean activated = false;
+    private SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,14 +44,61 @@ public class MainActivity extends AppCompatActivity {
          txtLogicoDicaInterval.setText(R.string.txtDicaDoIntevalo);
 
          timePicker.setIs24HourView(true);
+
+         preferences = getSharedPreferences("dataBase", Context.MODE_PRIVATE);
+         activated = preferences.getBoolean("activated",false);
+
+         if(activated){
+             btnNotify.setText(R.string.pause);
+             btnNotify.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.black));
+
+             int hour = preferences.getInt("hour", timePicker.getCurrentHour());
+             int minute = preferences.getInt("hour", timePicker.getCurrentMinute());
+             int interval = preferences.getInt("interval",0);
+
+             timePicker.setCurrentHour(hour);
+             timePicker.setCurrentMinute(minute);
+             editText.setText(String.valueOf(interval));
+         }
+
     }
 
     public void notifyClick(View view){
         String sInterval = editText.getText().toString();
 
+        if(sInterval.isEmpty()){
+            Toast.makeText(this,R.string.error_msg, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         hour = timePicker.getCurrentHour();
         minute = timePicker.getCurrentMinute();
         interval = Integer.parseInt(sInterval);
+
+        if(!activated){
+            btnNotify.setText(R.string.pause);
+            btnNotify.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.black));
+            activated=true;
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("activated", true);
+            editor.putInt("hour",hour);
+            editor.putInt("minute",minute);
+            editor.putInt("interval",interval);
+            editor.apply();
+
+        }else{
+            btnNotify.setText(R.string.txtBtnNotify);
+            btnNotify.setBackgroundTintList(ContextCompat.getColorStateList(this, R.color.colorPrimary));
+            activated=false;
+
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putBoolean("activated",false);
+            editor.remove("hour");
+            editor.remove("minute");
+            editor.remove("interval");
+            editor.apply();
+        }
 
         Log.d("teste","Hora: "+hour+", minutos: "+minute+", interval: "+interval+"min");
     }
